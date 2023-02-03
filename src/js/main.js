1312bb23b341f43ebbb23fe34aa89237a158fa70
Daniel Perez <danielperezcera@gmail.com>
2023-02-02 23:17:36 -0500

    Added Dynamic Header and Footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,13 @@
 // imports
+import {loadHeaderFooter} from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+
+loadHeaderFooter();
 
 // instantiate ProductData
 const dataSource = new ProductData("tents");
 const element = document.querySelector(".product-list");
 const productList = new ProductList("tents", dataSource, element);
 
-console.log(productList.init());
+productList.init()
