919ff68cce4e086dfc8d924e4c372f990b6809a7
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-02-24 01:54:56 -0400

    added option to modify quantity of products

--- src/js/ShoppingCart.js
+++ src/js/ShoppingCart.js
@@ -1,4 +1,4 @@
-import { getLocalStorage } from "./utils.mjs";
+import { setLocalStorage ,getLocalStorage, alertMessage } from "./utils.mjs";
 
 export default class ShoppingCart {
   constructor(key, parentSelector) {
@@ -22,6 +22,20 @@ export default class ShoppingCart {
 
       document.querySelector(this.parentSelector).innerHTML =
         htmlItems.join("");
+      
+        let minus = document.querySelectorAll('.minus');
+        minus.forEach((element) => {
+          element.addEventListener('click', (item) =>{
+              modifyQuantity(item.target.parentElement.id,'subtract');
+          });
+        });
+
+        let plus = document.querySelectorAll('.plus');
+        plus.forEach((element) => {
+          element.addEventListener('click', (item) =>{
+            modifyQuantity(item.target.parentElement.id,'sum');
+          });
+        });
     }
   }
 }
@@ -38,9 +52,68 @@ function cartItemTemplate(item) {
       <h2 class="card__name">${item.Name}</h2>
     </a>
     <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-    <p class="cart-card__quantity">qty: ${item.quantity}</p>
+    <p class="cart-card__quantity" id="${item.Id}">qty: <button type="button" class="sm-btn minus">-</button> <span class="qty${item.Id}">${item.quantity}</span> <button type="button" class="sm-btn plus">+</button></p>
     <p class="cart-card__price">$${item.FinalPrice * item.quantity}</p>
   </li>`;
 
   return newItem;
+}
+
+function modifyQuantity(productId,option) {
+  // const query = getLocalStorage("so-cart");
+  let cart = getLocalStorage("so-cart");
+
+  if (!cart) {
+    cart = [];
+  }
+
+  // if it exists, only add to its quantity
+  // else we need to push it into the array after the check
+  let alreadyExists = false;
+
+  if (cart) {
+    // for each, check if the cartItem id is already present
+    cart.forEach((cartItem) => {
+      // just in case we are using an old saved localstorage list
+      // it adds the quantity property
+      if (!cartItem.quantity) {
+        cartItem.quantity = 1;
+      }
+
+      // If we already have one with the same id, add to it
+      if (cartItem.Id === productId) {
+        alreadyExists = true;
+
+        if(option == "sum"){
+          cartItem.quantity++;
+          alertMessage(`Added to the cart!`);
+        }else if(option == "subtract" && cartItem.quantity > 0){
+          cartItem.quantity--;
+          alertMessage(`Subtracted from the cart!`);
+        }
+        setLocalStorage("so-cart", cart);
+        
+        const newQty = cartItem.quantity;
+
+        const elem = document.querySelector(".qty"+productId);
+        elem.textContent = newQty;
+        const newTotal = "$" + (cartItem.FinalPrice * newQty);
+        elem.parentElement.nextSibling.nextSibling.textContent = newTotal;
+      }
+    });
+  }
+  // don't want to mutate an array while traversing it
+  // if the item doesn't exist yet...
+  if (!alreadyExists) {
+    // create a temp newCartItem
+    const newCartItem = this.product;
+    // add the quantity property to it and assign a value of 1
+    newCartItem.quantity = 1;
+    cart.push(newCartItem);
+    alertMessage(`${this.product.Name} was added to the cart!`);
+  }
+
+  setLocalStorage("so-cart", cart);
+
+  
 }
